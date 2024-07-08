b74e6d15303a62977592fe28be8ff9ff050ccd3f
smnunezleal <sebastian1986@gmail.com>
2024-07-08 15:09:01 -0300

    cambios sobre lote y bloque.

--- src/RecetasW3.sol
+++ src/RecetasW3.sol
@@ -12,23 +12,28 @@ contract RecetasW3 {
         bool dispensada;
         string DIDfarmacia;
         Medicamento[] medicamentos;
+        string lote; // Nuevo campo para el lote
+        uint256 bloque; // Nuevo campo para el bloque de la transacción
     }
 
     mapping(string => Receta) public recetas;
 
     event RecetaEmitida(string hash);
-    event RecetaDispensada(string hash, string DIDfarmacia);
+    event RecetaDispensada(string hash, string DIDfarmacia, string lote);
 
     function emitirReceta(
         string memory _hash,
         Medicamento[] memory _medicamentos
     ) public {
+        require(bytes(_hash).length != 0, unicode"El hash no puede estar vacío.");
         require(bytes(recetas[_hash].hash).length == 0, unicode"La receta ya ha sido emitida.");
         
         Receta storage receta = recetas[_hash];
         receta.hash = _hash;
         receta.dispensada = false;
         receta.DIDfarmacia = "";
+        receta.lote = ""; // Inicialmente vacío
+        receta.bloque = block.number; // Guardar el bloque de la transacción
 
         for (uint i = 0; i < _medicamentos.length; i++) {
             receta.medicamentos.push(Medicamento({
@@ -40,19 +45,19 @@ contract RecetasW3 {
         emit RecetaEmitida(_hash);
     }
 
-    function dispensarMedicamento(string memory _hash, string memory _DIDfarmacia) public {
+    function dispensarMedicamento(string memory _hash, string memory _DIDfarmacia, string memory _lote) public {
+        Receta storage receta = recetas[_hash];
         require(bytes(recetas[_hash].hash).length != 0, unicode"La receta no existe.");
-        require(!recetas[_hash].dispensada, unicode"La receta ya fue dispensada.");
+        require(!receta.dispensada, unicode"La receta ya fue dispensada.");
         
-        recetas[_hash].DIDfarmacia = _DIDfarmacia;
-        recetas[_hash].dispensada = true;
+        receta.DIDfarmacia = _DIDfarmacia;
+        receta.lote = _lote; // Completar el campo lote
+        receta.dispensada = true;
 
-        emit RecetaDispensada(_hash, _DIDfarmacia);
+        emit RecetaDispensada(_hash, _DIDfarmacia, _lote);
     }
 
-
     function verificarReceta(string memory _hash) public view returns (Receta memory) {
-        Receta memory receta = recetas[_hash];
-        return receta;
+        return recetas[_hash];
     }
 }
